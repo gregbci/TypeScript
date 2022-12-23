1b17282de945d635d7be0ff318b3961bfb9bd4de
Greg Wilding <gregory.wilding@ucalgary.ca>
2022-12-23 10:35:47 -0700

    Updating event handler based on eslint feedback
    - Function is like 'any', be explicit about the signature of the callback instead
    - Refactored the arrow function into a real function with signature needed for event
    - One significant headache with Electron is the use of events for IPC, there's no type info

--- main.ts
+++ main.ts
@@ -15,29 +15,32 @@ function createWindow() {
    // Device scans start with calling Bluetooth.requestDevice(...) on the renderer side.  
    // When devices are found, this event is triggered.  We can use this event to present 
    // a device picker to the user, or we can automaically pick something.
-   win.webContents.on('select-bluetooth-device', (event: Event, devices: BluetoothDevice[], callback: Function) => {
-      event.preventDefault();
-
-      // just pick the first device discovered (requestDevice() handles filtering)
-      if (devices && devices.length > 0) {
-         callback(devices[0].deviceId);
-      }
-   });
+   win.webContents.on('select-bluetooth-device', bluetoothSelected);
 
    win.loadFile('index.html');
+}
+
+
+function bluetoothSelected(event: Event, devices: BluetoothDevice[], callback: (deviceId: string) => void) {
+   event.preventDefault();
+
+   // just pick the first device discovered (requestDevice() handles filtering)
+   if (devices && devices.length > 0) {
+      callback(devices[0].deviceId);
+   }
 }
 
 app.whenReady().then(() => {
    createWindow();
 
-   app.on('activate', function () {
+   app.on('activate', () => {
       if (BrowserWindow.getAllWindows().length === 0) {
          createWindow();
       }
    });
 });
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
    console.log("Bye");
    app.quit();
 });
