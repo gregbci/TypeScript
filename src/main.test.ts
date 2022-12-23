51278d5a06023c2afbccd2d8a94fcd253a19a369
Greg Wilding <gregory.wilding@ucalgary.ca>
2022-12-23 14:27:52 -0700

    Jest tests written in TypeScript now work
    - the trick was telling Jest to use ts-jest to transpile the .test.ts files before parsing.  Otherwise it was just treating .test.ts files as JavaScript and blowing up.

--- src/main.test.ts
+++ src/main.test.ts
@@ -4,22 +4,22 @@ export const addTwoNumbers = (first: number, second: number) => {
    return first + second;
 };
 
-// function percentError(initial: number, final: number) {
-//    let result = undefined;
-//    if (initial > 0) {
-//       result = (initial - final) / initial * 100.0
-//    }
-//    return result;
-// }
+function percentError(initial: number, final: number) {
+   let result = undefined;
+   if (initial > 0) {
+      result = (initial - final) / initial * 100.0
+   }
+   return result;
+}
 
-// describe('Dummy tests', () => {
+describe('Dummy tests', () => {
 
-//    test('Calculating a percentage', () => {
-//       expect(percentError(10, 1)).toBe(90.0);
-//    });
+   test('Calculating a percentage', () => {
+      expect(percentError(10, 1)).toBe(90.0);
+   });
 
-//    test('Cannot calculate percent with invaid input', () => {
-//       expect(percentError(-10, 1)).toBe(undefined);
-//    });
+   test('Cannot calculate percent with invaid input', () => {
+      expect(percentError(-10, 1)).toBe(undefined);
+   });
 
-// });
+});
